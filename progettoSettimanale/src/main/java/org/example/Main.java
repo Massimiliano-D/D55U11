a6ae289c5a07edee3f702100c67365d717023903
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-13 15:49:45 +0200

    es. 3-4-5 eseguiti e testati
    eseguiti e testati esercizi di ricerca per: ISBM, ANNO e AUTORE

--- progettoSettimanale/src/main/java/org/example/Main.java
+++ progettoSettimanale/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -28,8 +30,16 @@ public class Main {
         System.out.println("************AGGIUNTA************");
         addElem(lib1, pippoCatalogo);
         System.out.println(pippoCatalogo);
+        System.out.println("************Ricerca per ANNO************");
+        srcAnno(pippoCatalogo, 2012);
+        System.out.println("************Ricerca per AUTORE************");
+        srcAutore(pippoCatalogo, "Andrzej Sapkowsk");
+        System.out.println("************Ricerca per ISBM************");
+        srcIsbn(pippoCatalogo, "853698");
     }
 
+
+    //************AGGIUNTA************\\
     static void addElem(Catalogo elem, Set<Catalogo> contenitore) {
         if (contenitore.add(elem)) {
             System.out.println("AGGIUNTO CON SUCCESSO!!");
@@ -38,6 +48,8 @@ public class Main {
         }
     }
 
+
+    //************RIMOZIONE************\\
     static void remElem(String Isbm, Set<Catalogo> contenitore) {
         if (contenitore.removeIf(elm -> elm.getCodiceISBM().equals(Isbm))) {
             System.out.println("codice:" + " " + Isbm + " " + "ELIMINATO!!");
@@ -45,4 +57,41 @@ public class Main {
             System.out.println("CODICE NON TROVATO!!");
         }
     }
+
+
+    //************Ricerca per ANNO************\\
+    static void srcAnno(Set<Catalogo> contenitore, int anno) {
+        Set<Catalogo> pippoFiltro = contenitore.stream().filter(ey -> ey.getAnnoPubblica() == anno).collect(Collectors.toSet());
+        System.out.println("Elementi trovati:" + " " + anno);
+        System.out.println("*************");
+        for (Catalogo elem : pippoFiltro) {
+            System.out.println(elem);
+        }
+    }
+
+
+    //************Ricerca per ISBN************\\
+    static void srcIsbn(Set<Catalogo> contenitore, String isbn) {
+        Set<Catalogo> pippoFiltro = contenitore.stream().filter(isb -> Objects.equals(isb.getCodiceISBM(), isbn)).collect(Collectors.toSet());
+        System.out.println("Elementi trovati con codice:" + " " + isbn);
+        System.out.println("*************");
+        for (Catalogo elem : pippoFiltro) {
+            System.out.println(elem);
+        }
+    }
+
+
+    //************Ricerca per AUTORE************\\
+    static void srcAutore(Set<Catalogo> contenitore, String autore) {
+        Set<Catalogo> pippoFiltrato = contenitore.stream()
+                .filter(auto -> auto instanceof Libro)
+                .filter(auto -> ((Libro) auto).getAutore().equalsIgnoreCase(autore))
+                .collect(Collectors.toSet());
+        System.out.println("elementi trovati con autore: " + autore);
+        System.out.println("**********");
+        for (Catalogo elemento : pippoFiltrato) {
+            System.out.println(elemento);
+        }
+    }
+
 }
